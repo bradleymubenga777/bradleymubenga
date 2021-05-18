d80fadb7ed94ec75151bf1b7ddb50b6560129758
Bradley Mubenga <bradleymubenga777@gmail.com>
2021-05-18 20:35:19 +0200

    Strategy Section Complete

--- client/src/components/component/home/heroSection.jsx
+++ client/src/components/component/home/heroSection.jsx
@@ -2,19 +2,20 @@
 import React from 'react';
 
 //SCSS
-import '../../scss/heroSection.scss';
+import '../../../scss/heroSection.scss';
 
 //Assets
-import backgroundVideo from '../../assets/video/bradley-mubenga-web-developer-video.m4v';
+import backgroundVideo from '../../../assets/video/bradley-mubenga-web-developer-video.m4v';
+import downArrow from '../../../assets/svg/down-arrow.svg';
 
 //React Components
-import { WhiteBtn, BlackBtn } from '../buttons/buttons';
+import { WhiteBtn, BlackBtn } from '../../buttons/buttons';
 
 export default function HeroSection() {
     return (
     <section className="jumbotron jumbotron-fluid heroSection d-flex flex-column justify-content-center align-items-center">
         <div className="text-center container heroText">
-            <h1 className="display-4">Professional Web Design for Busy Entreprenurs</h1>
+            <h1 className="display-4">Professional Web Design for Busy Entrepreneurs</h1>
 
             <div className="d-flex justify-content-center align-items-center g-5 mt-4">
                 <WhiteBtn />
--- client/src/components/pages/home.jsx
+++ client/src/components/pages/home.jsx
@@ -2,12 +2,14 @@
 import React from 'react';
 
 //React Components
-import HeroSection from '../component/heroSection';
+import HeroSection from '../component/home/heroSection';
+import StrategySection from '../component/home/strategySection';
 
 export default function Home() {
     return (
         <main>
             <HeroSection />
+            <StrategySection />
         </main>
     )
 }
