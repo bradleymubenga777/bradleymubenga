8582f811e63f4def11683aefcf74870c31fddcd4
Bradley Mubenga <bradleymubenga777@gmail.com>
2021-05-22 19:49:49 +0200

    Home Page Process Section Added

--- client/src/components/pages/home.jsx
+++ client/src/components/pages/home.jsx
@@ -6,12 +6,14 @@ import HeroSection from '../component/home/heroSection';
 import StrategySection from '../component/home/strategySection';
 import NeedProof from '../component/home/needProof';
 import ContactSection from '../component/contact/startProject';
+import TheProcess from '../component/home/theProcess';
 
 export default function Home() {
     return (
         <main>
             <HeroSection />
             <StrategySection />
+            <TheProcess />
             <NeedProof />
             <ContactSection />
         </main>
