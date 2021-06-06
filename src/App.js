f1001776484e6871999a06f70af0189179fa9fb7
Bradley Mubenga <bradleymubenga777@gmail.com>
2021-06-06 17:30:06 +0200

    Responsive Init Commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 //SCSS
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './scss/branding.scss';
+import './scss/responsive.scss';
 
 //Components
 import NavBar from './components/navigation/navbar';
